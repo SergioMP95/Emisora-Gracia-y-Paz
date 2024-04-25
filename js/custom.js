a3cd7bb91d0778fad96c12c77e55f063ee45cff5
SergioMP95 <elektrik9500@gmail.com>
2024-04-25 07:46:27 -0500

    Estilos 98% finalizados, correcciones realizadas

--- js/custom.js
+++ js/custom.js
@@ -35,36 +35,65 @@ function handlePlayButton() {
 }
 
 $(document).ready(function(){
-    $('.director__slider').slick({
-      autoplay: true,
-      arrows: false,
-      dots: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      responsive: 
-      [
-        {
-          breakpoint: 1023,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          }
-        },
-        {
-          breakpoint: 719,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          }
+  $('.collab__slider').slick({
+    autoplay: true,
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: 
+    [
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
         }
-      ]
-    });  
+      },
+      {
+        breakpoint: 719,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });  
 })
 
 $(document).ready(function(){
-  $('.collab__slider').slick({
+  $('.director__slider').slick({
+    autoplay: true,
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: 
+    [
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 719,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });  
+})
+
+$(document).ready(function(){
+  $('.announcers__slider').slick({
     autoplay: true,
     arrows: false,
     dots: false,
