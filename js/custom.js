743a0e9962686d6ba5064a5b61be142f714ef429
SergioMP95 <elektrik9500@gmail.com>
2024-04-24 20:09:38 -0500

    Agregada la estructura de locutores

--- js/custom.js
+++ js/custom.js
@@ -10,24 +10,84 @@ close.addEventListener("click", () => {
     menu.classList.remove("visible")
 })
 
-$(document).ready(function(){
-    $('.collab__slider').slick({
-      arrows: false,
-      dots: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    });  
-  })
+let videoElem = document.getElementById("video");
+let playButton = document.getElementById("playbutton");
 
-  $(document).ready(function(){
+playButton.addEventListener("click", handlePlayButton, false);
+playVideo();
+
+async function playVideo() {
+  try {
+    await videoElem.play();
+    playButton.className = "playing";
+  } catch (err) {
+    playButton.className = "";
+  }
+}
+
+function handlePlayButton() {
+  if (videoElem.paused) {
+    playVideo();
+  } else {
+    videoElem.pause();
+    playButton.className = "";
+  }
+}
+
+$(document).ready(function(){
     $('.director__slider').slick({
+      autoplay: true,
       arrows: false,
       dots: false,
       infinite: true,
       speed: 300,
-      slidesToShow: 1,
+      slidesToShow: 3,
       slidesToScroll: 1,
+      responsive: 
+      [
+        {
+          breakpoint: 1023,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 719,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
     });  
-  })
+})
+
+$(document).ready(function(){
+  $('.collab__slider').slick({
+    autoplay: true,
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: 
+    [
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 719,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });  
+})
