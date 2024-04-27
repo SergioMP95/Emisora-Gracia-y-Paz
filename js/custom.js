21888cc10b4d35885496dc523b00bd9b3b4224f3
SergioMP95 <elektrik9500@gmail.com>
2024-04-26 22:41:54 -0500

    correcion de sliders

--- js/custom.js
+++ js/custom.js
@@ -10,29 +10,6 @@ close.addEventListener("click", () => {
     menu.classList.remove("visible")
 })
 
-let videoElem = document.getElementById("video");
-let playButton = document.getElementById("playbutton");
-
-playButton.addEventListener("click", handlePlayButton, false);
-playVideo();
-
-async function playVideo() {
-  try {
-    await videoElem.play();
-    playButton.className = "playing";
-  } catch (err) {
-    playButton.className = "";
-  }
-}
-
-function handlePlayButton() {
-  if (videoElem.paused) {
-    playVideo();
-  } else {
-    videoElem.pause();
-    playButton.className = "";
-  }
-}
 
 $(document).ready(function(){
   $('.collab__slider').slick({
